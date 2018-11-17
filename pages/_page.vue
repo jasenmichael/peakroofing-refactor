85fe6f02083b37205773d328f2d34ec0c3c59899
jasenmichael <jasen@ready-builder.com>
2018-11-17 15:32:09 -0700

    impllement prefetch pageContent

--- pages/_page.vue
+++ pages/_page.vue
@@ -2,8 +2,8 @@
     <section class="container">
       <!-- when I uncomment this it works because the state is populated
       but if I hard refresh it is broken -->
-      <!-- <h1 v-html='pageContent.title' />
-      <div v-html='pageContent.body.value' /> -->
+      <h1 v-html='pageContent.title' />
+      <div v-html='pageContent.body.value' />
       <hr>
       <h2>this is a {{ type }} with the name: {{ name }}, and path: {{ path }} </h2>
       <pre> {{ pageContent }} </pre>
@@ -20,24 +20,16 @@ export default {
       name: this.$nuxt.$route.params.page,
       path: this.$nuxt.$route.path,
       type: this.$nuxt.$route.name,
-      pageContent: this.$store.state.pages[this.$nuxt.$route.params.page]
+      pageContent: this.$store.state.pages[this.$nuxt.$route.params.page],
     }
   },
-  created() {
-    this.getPageContentByPath(this.path)
-  },
-  methods: {
-    getPageContentByPath(path) {
-      if (this.$store.state.pages[this.name] === undefined ) {
-      return getPageContentByPath(this.path)
+  fetch({store, route}) {
+    if (store.state.pages[route.params.page] === undefined ) {
+      return getPageContentByPath(route.fullPath)
         .then((res) => {
-        this.$store.state.pages[this.name] = res.data[0].attributes
-        this.pageContent = this.$store.state.pages[this.name]
-        })
-      }
-      else {
-        this.pageContent = this.$store.state.pages[this.name]
-      }
+        store.state.pages[route.params.page] = res.data[0].attributes
+        this.pageContent = store.state.pages[route.params.page]
+      })
     }
   }
 }
