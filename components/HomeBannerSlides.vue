f57c74f812468368a7d93c92dfabab37dfa6f0b3
jasenmichael <jasen@ready-builder.com>
2018-11-18 15:29:22 -0700

    display images on page

--- components/HomeBannerSlides.vue
+++ components/HomeBannerSlides.vue
@@ -3,6 +3,12 @@
     <br><br><br>
     <h1>homeBannerSlides yo!</h1>
     <pre v-html="homeBannerSlides" />
+
+    <div class="slider">
+      <li v-for="slide in homeBannerSlides" v-bind:key="slide.id">
+                <img class="scroll" :src="slide.url"/>
+      </li>
+    </div>  
   </div>
 </template>
 
@@ -12,9 +18,9 @@ import { getImageGalleryByPath, getFileByUuid } from '~/lib/api'
 
 export default {
   data() {
-  return {
-    homeBannerSlides: this.$store.state.galleries.homeBannerSlides
-    }
+    return {
+      homeBannerSlides: this.$store.state.galleries.homeBannerSlides
+      }
   },
   created() {
     this.getImageGalleryByPath("/gallery/main-banner-slideshow")
