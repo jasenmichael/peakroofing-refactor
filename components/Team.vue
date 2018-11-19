32169769b0b40a120e437f2249588539d93ce78c
jasenmichael <jasen@ready-builder.com>
2018-11-18 21:59:01 -0700

    create Team component

---
+++ components/Team.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <br><br><br>
+    <h1>Bad Ass Team yo!</h1>
+    <pre v-html="team" />
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+import { getTeam } from '~/lib/api'
+
+export default {
+  data() {
+    return {
+      team: this.$store.state.team
+      }
+  },
+  created() {
+    this.getTeam()
+  },
+  methods: {
+    getTeam() {
+      if (!this.team) {
+      return getTeam()
+        .then((res) => {
+        this.$store.state.team = res
+        this.team = res
+        })
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
